412d8bfdcd30a301768107c86b1998e42f7bc82f
xiaolei <coder_xiaolei@163.com>
2017-04-24 16:10:03 +0800

    初始化工程

---
+++ JSMethodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSMethodTest
+//
+//  Created by Madis on 2017/4/24.
+//  Copyright © 2017年 xl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSMethodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSMethodTest
+//
+//  Created by Madis on 2017/4/24.
+//  Copyright © 2017年 xl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
